ccf9336f2f906d316302b858d4493bfd9b06242f
Michał Lytek <michal.ly@wp.pl>
2018-12-30 18:03:42 +0100

    Fix types inconsistency

--- src/data.ts
+++ src/data.ts
@@ -1,9 +1,21 @@
-export const projects = [
+export interface ProjectData {
+  id: number;
+  name: string;
+}
+
+export interface TaskData {
+  id: number;
+  title: string;
+  completed: boolean;
+  project_id: number;
+}
+
+export const projects: ProjectData[] = [
   { id: 1, name: "Learn React Native" },
   { id: 2, name: "Workout" },
 ];
 
-export const tasks = [
+export const tasks: TaskData[] = [
   { id: 1, title: "Install Node", completed: true, project_id: 1 },
   {
     id: 2,
